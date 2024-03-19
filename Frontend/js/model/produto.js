523aef932c254208cc75ddcdba7e5a6f94927ff2
Pedro Reis <67246076+PedroReis98@users.noreply.github.com>
2024-03-19 19:53:23 -0300

    Add files via upload

---
+++ Frontend/js/model/produto.js
@@ -0,0 +1,15 @@
+class Produto {
+    constructor(obj){
+        obj = obj || {}// Isso é um tratamento para evitar undefined quando tentar acessar alguma propriedade.
+        this.id = obj.id;
+        this.nome = obj.nome;
+        this.valor = obj.valor;
+        this.quantidadeEstoque = obj.quantidadeEstoque;
+        this.observacao = obj.observacao;
+        this.dataCadastro = obj.dataCadastro;
+    }
+
+    validar(){
+      return !!(this.nome && this.quantidadeEstoque);  
+    }
+}
